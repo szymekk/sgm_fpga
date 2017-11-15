10d64865d23f5647bca9af6e6d3494ce300a582d
Szymon Kozłowski <szymek.kozlowski1@gmail.com>
2017-11-16 00:07:28 +0100

    Add block design and ip repo

---
+++ block_design/hdmi_vga_block_design/hdl/hdmi_vga_block_design.v
@@ -0,0 +1,126 @@
+//Copyright 1986-2016 Xilinx, Inc. All Rights Reserved.
+//--------------------------------------------------------------------------------
+//Tool Version: Vivado v.2016.3 (win64) Build 1682563 Mon Oct 10 19:07:27 MDT 2016
+//Date        : Wed Nov 15 23:33:12 2017
+//Host        : komputerek running 64-bit major release  (build 9200)
+//Command     : generate_target hdmi_vga_block_design.bd
+//Design      : hdmi_vga_block_design
+//Purpose     : IP block netlist
+//--------------------------------------------------------------------------------
+`timescale 1 ps / 1 ps
+
+(* CORE_GENERATION_INFO = "hdmi_vga_block_design,IP_Integrator,{x_ipVendor=xilinx.com,x_ipLibrary=BlockDiagram,x_ipName=hdmi_vga_block_design,x_ipVersion=1.00.a,x_ipLanguage=VERILOG,numBlks=4,numReposBlks=4,numNonXlnxBlks=2,numHierBlks=0,maxHierDepth=0,numSysgenBlks=0,numHlsBlks=0,numHdlrefBlks=0,numPkgbdBlks=0,bdsource=USER,synth_mode=OOC_per_IP}" *) (* HW_HANDOFF = "hdmi_vga_block_design.hwdef" *) 
+module hdmi_vga_block_design
+   (hdmi_hpd,
+    hdmi_in_clk_n,
+    hdmi_in_clk_p,
+    hdmi_in_data_n,
+    hdmi_in_data_p,
+    hdmi_in_ddc_scl_i,
+    hdmi_in_ddc_scl_o,
+    hdmi_in_ddc_scl_t,
+    hdmi_in_ddc_sda_i,
+    hdmi_in_ddc_sda_o,
+    hdmi_in_ddc_sda_t,
+    sys_clock,
+    vga_pBlue,
+    vga_pGreen,
+    vga_pHSync,
+    vga_pRed,
+    vga_pVSync);
+  output [0:0]hdmi_hpd;
+  input hdmi_in_clk_n;
+  input hdmi_in_clk_p;
+  input [2:0]hdmi_in_data_n;
+  input [2:0]hdmi_in_data_p;
+  input hdmi_in_ddc_scl_i;
+  output hdmi_in_ddc_scl_o;
+  output hdmi_in_ddc_scl_t;
+  input hdmi_in_ddc_sda_i;
+  output hdmi_in_ddc_sda_o;
+  output hdmi_in_ddc_sda_t;
+  input sys_clock;
+  output [4:0]vga_pBlue;
+  output [5:0]vga_pGreen;
+  output vga_pHSync;
+  output [4:0]vga_pRed;
+  output vga_pVSync;
+
+  wire clk_wiz_0_clk_out1;
+  wire dvi2rgb_0_DDC_SCL_I;
+  wire dvi2rgb_0_DDC_SCL_O;
+  wire dvi2rgb_0_DDC_SCL_T;
+  wire dvi2rgb_0_DDC_SDA_I;
+  wire dvi2rgb_0_DDC_SDA_O;
+  wire dvi2rgb_0_DDC_SDA_T;
+  wire dvi2rgb_0_PixelClk;
+  wire dvi2rgb_0_RGB_ACTIVE_VIDEO;
+  wire [23:0]dvi2rgb_0_RGB_DATA;
+  wire dvi2rgb_0_RGB_HSYNC;
+  wire dvi2rgb_0_RGB_VSYNC;
+  wire hdmi_in_1_CLK_N;
+  wire hdmi_in_1_CLK_P;
+  wire [2:0]hdmi_in_1_DATA_N;
+  wire [2:0]hdmi_in_1_DATA_P;
+  wire [4:0]rgb2vga_0_vga_pBlue;
+  wire [5:0]rgb2vga_0_vga_pGreen;
+  wire rgb2vga_0_vga_pHSync;
+  wire [4:0]rgb2vga_0_vga_pRed;
+  wire rgb2vga_0_vga_pVSync;
+  wire sys_clock_1;
+  wire [0:0]xlconstant_0_dout;
+
+  assign dvi2rgb_0_DDC_SCL_I = hdmi_in_ddc_scl_i;
+  assign dvi2rgb_0_DDC_SDA_I = hdmi_in_ddc_sda_i;
+  assign hdmi_hpd[0] = xlconstant_0_dout;
+  assign hdmi_in_1_CLK_N = hdmi_in_clk_n;
+  assign hdmi_in_1_CLK_P = hdmi_in_clk_p;
+  assign hdmi_in_1_DATA_N = hdmi_in_data_n[2:0];
+  assign hdmi_in_1_DATA_P = hdmi_in_data_p[2:0];
+  assign hdmi_in_ddc_scl_o = dvi2rgb_0_DDC_SCL_O;
+  assign hdmi_in_ddc_scl_t = dvi2rgb_0_DDC_SCL_T;
+  assign hdmi_in_ddc_sda_o = dvi2rgb_0_DDC_SDA_O;
+  assign hdmi_in_ddc_sda_t = dvi2rgb_0_DDC_SDA_T;
+  assign sys_clock_1 = sys_clock;
+  assign vga_pBlue[4:0] = rgb2vga_0_vga_pBlue;
+  assign vga_pGreen[5:0] = rgb2vga_0_vga_pGreen;
+  assign vga_pHSync = rgb2vga_0_vga_pHSync;
+  assign vga_pRed[4:0] = rgb2vga_0_vga_pRed;
+  assign vga_pVSync = rgb2vga_0_vga_pVSync;
+  hdmi_vga_block_design_clk_wiz_0_0 clk_wiz_0
+       (.clk_in1(sys_clock_1),
+        .clk_out1(clk_wiz_0_clk_out1),
+        .reset(1'b0));
+  hdmi_vga_block_design_dvi2rgb_0_0 dvi2rgb_0
+       (.DDC_SCL_I(dvi2rgb_0_DDC_SCL_I),
+        .DDC_SCL_O(dvi2rgb_0_DDC_SCL_O),
+        .DDC_SCL_T(dvi2rgb_0_DDC_SCL_T),
+        .DDC_SDA_I(dvi2rgb_0_DDC_SDA_I),
+        .DDC_SDA_O(dvi2rgb_0_DDC_SDA_O),
+        .DDC_SDA_T(dvi2rgb_0_DDC_SDA_T),
+        .PixelClk(dvi2rgb_0_PixelClk),
+        .RefClk(clk_wiz_0_clk_out1),
+        .TMDS_Clk_n(hdmi_in_1_CLK_N),
+        .TMDS_Clk_p(hdmi_in_1_CLK_P),
+        .TMDS_Data_n(hdmi_in_1_DATA_N),
+        .TMDS_Data_p(hdmi_in_1_DATA_P),
+        .aRst(xlconstant_0_dout),
+        .pRst(1'b0),
+        .vid_pData(dvi2rgb_0_RGB_DATA),
+        .vid_pHSync(dvi2rgb_0_RGB_HSYNC),
+        .vid_pVDE(dvi2rgb_0_RGB_ACTIVE_VIDEO),
+        .vid_pVSync(dvi2rgb_0_RGB_VSYNC));
+  hdmi_vga_block_design_rgb2vga_0_0 rgb2vga_0
+       (.PixelClk(dvi2rgb_0_PixelClk),
+        .rgb_pData(dvi2rgb_0_RGB_DATA),
+        .rgb_pHSync(dvi2rgb_0_RGB_HSYNC),
+        .rgb_pVDE(dvi2rgb_0_RGB_ACTIVE_VIDEO),
+        .rgb_pVSync(dvi2rgb_0_RGB_VSYNC),
+        .vga_pBlue(rgb2vga_0_vga_pBlue),
+        .vga_pGreen(rgb2vga_0_vga_pGreen),
+        .vga_pHSync(rgb2vga_0_vga_pHSync),
+        .vga_pRed(rgb2vga_0_vga_pRed),
+        .vga_pVSync(rgb2vga_0_vga_pVSync));
+  hdmi_vga_block_design_xlconstant_0_0 xlconstant_0
+       (.dout(xlconstant_0_dout));
+endmodule
