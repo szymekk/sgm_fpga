4075c0beab1a7f3b122f64aa54efd9e1d271b238
Szymon Kozłowski <szymek.kozlowski1@gmail.com>
2017-12-07 16:01:14 +0100

    Implement matching costs truncation
    Matching cost value is now truncated to limit the number of bits used for
    carrying cost values.
    Also automated cost bit width calculation.
    SGM works in four directions.

--- rtl/simple_sgm.v
+++ rtl/simple_sgm.v
@@ -2,9 +2,9 @@ module simple_sgm
 # (
 //    localparam HALF_IMG_WIDTH = 400,
     localparam HALF_IMG_WIDTH = 640, // 1280/2
-    localparam DISPARITY_RANGE = 128
+//    localparam DISPARITY_RANGE = 128
 //    localparam DISPARITY_RANGE = 64
-//    localparam DISPARITY_RANGE = 32
+    localparam DISPARITY_RANGE = 32
 )
 (
     //inputs
@@ -42,20 +42,25 @@ for (i=0; i<MAX_DISP; i=i+1) begin : assign_delay
 end
 endgenerate
 
-localparam COST_BITS = 8;
+//localparam COST_BITS = 8;
+localparam COST_BITS = 6;
 //absolute difference calculation
+localparam [COST_BITS-1:0] THRESHOLD = {COST_BITS{1'b1}};
+//localparam MAX_COST = 255;
+localparam MAX_COST = THRESHOLD;
+localparam test = {COST_BITS{1'b1}};
+wire [7:0] pixelwise_abs_diff [0:MAX_DISP];
 wire [COST_BITS-1:0] matching_cost [0:MAX_DISP];
 generate
 for (i=0; i<=MAX_DISP; i=i+1) begin : calculate_abs_diff
-    assign matching_cost[i] = (pixel_left > right_arr[i]) ? (pixel_left - right_arr[i]) : (right_arr[i] - pixel_left);
+    assign pixelwise_abs_diff[i] = (pixel_left > right_arr[i]) ? (pixel_left - right_arr[i]) : (right_arr[i] - pixel_left);
+//    assign matching_cost[i] = pixelwise_abs_diff[i];
+    // truncated matching cost
+    assign matching_cost[i] = (pixelwise_abs_diff[i] > THRESHOLD) ? THRESHOLD : pixelwise_abs_diff[i];
 end
 endgenerate
 
 localparam ARR_WIDTH = COST_BITS*DISPARITY_RANGE;
-/*
-wire [ARR_WIDTH-1:0] array = {matching_cost[7], matching_cost[6], matching_cost[5], matching_cost[4],
-                              matching_cost[3], matching_cost[2], matching_cost[1], matching_cost[0]};//todo
-*/
 wire [ARR_WIDTH-1:0] matching_costs_array;//todo
 genvar d;
 generate
@@ -84,7 +89,11 @@ wire half_img_de = de_in && (col >= HALF_IMG_WIDTH);
 
 wire horizontal_beginning = (0 + HALF_IMG_WIDTH == col);
 
-localparam ACC_COST_BITS = COST_BITS + 1;//todo
+localparam [7:0] MAX_P1 = 8'd15;
+localparam [7:0] MAX_P2 = 8'd64;
+localparam MAX_ACC_COST = MAX_COST + MAX_P2;
+localparam ACC_COST_BITS = clog2(MAX_ACC_COST + 1);
+//localparam ACC_COST_BITS = COST_BITS + 1;
 localparam PATH_COST_ARR_WIDTH = ACC_COST_BITS*DISPARITY_RANGE;
 
 wire [PATH_COST_ARR_WIDTH-1:0] L_array_horizontal;
@@ -98,8 +107,8 @@ path_cost_calculator #(
     //.in_de(half_img_de), // todo remove?
     .in_path_beginning(horizontal_beginning),
     .in_clk(clk),
-    .in_P1(8'd15), // todo variable
-    .in_P2(8'd100), // todo P2 calculation
+    .in_P1(MAX_P1),
+    .in_P2(MAX_P2), // todo P2 calculation
     .in_C_arr(matching_costs_array), // width = COST_BITS*(MAX_DISP + 1)
     //outputs
     .out_L_arr(L_array_horizontal) // width = ACC_COST_BITS*(MAX_DISP + 1)
@@ -129,14 +138,14 @@ path_cost_calculator #(
     .in_de(extended_half_img_de), // extended de signal
     .in_path_beginning(diagonal_left_to_right_beginning), // 402 (401 effective)
     .in_clk(clk),
-    .in_P1(8'd15), // todo variable
-    .in_P2(8'd100), // todo P2 calculation
+    .in_P1(MAX_P1),
+    .in_P2(MAX_P2), // todo P2 calculation
     .in_C_arr(matching_costs_array), // width = COST_BITS*(MAX_DISP + 1)
     //outputs
     .out_L_arr(L_array_left_to_right) // width = ACC_COST_BITS*(MAX_DISP + 1)
 );
 
-/*
+
 wire [PATH_COST_ARR_WIDTH-1:0] L_array_top_to_bottom;
 path_cost_calculator #(// WORKING
     .DISPARITY_LEVELS(DISPARITY_RANGE),
@@ -149,14 +158,14 @@ path_cost_calculator #(// WORKING
     .in_de(extended_half_img_de), // test extended de signal
     .in_path_beginning(top_to_bottom_beginning), // 401 (400 effective)
     .in_clk(clk),
-    .in_P1(8'd15), // todo variable
-    .in_P2(8'd100), // todo P2 calculation
+    .in_P1(MAX_P1),
+    .in_P2(MAX_P2), // todo P2 calculation
     .in_C_arr(matching_costs_array), // width = COST_BITS*(MAX_DISP + 1)
     //outputs
     .out_L_arr(L_array_top_to_bottom) // width = ACC_COST_BITS*(MAX_DISP + 1)
 );
-*/
-/*
+
+
 wire [PATH_COST_ARR_WIDTH-1:0] L_array_right_to_left;
 path_cost_calculator #(// WORKING
     .DISPARITY_LEVELS(DISPARITY_RANGE),
@@ -169,27 +178,30 @@ path_cost_calculator #(// WORKING
     .in_de(extended_half_img_de), // extended de signal
     .in_path_beginning(diagonal_right_to_left_beginning), // 400 (399 effective)
     .in_clk(clk),
-    .in_P1(8'd15), // todo variable
-    .in_P2(8'd100), // todo P2 calculation
+    .in_P1(MAX_P1),
+    .in_P2(MAX_P2), // todo P2 calculation
     .in_C_arr(matching_costs_array), // width = COST_BITS*(MAX_DISP + 1)
     //outputs
     .out_L_arr(L_array_right_to_left) // width = ACC_COST_BITS*(MAX_DISP + 1)
 );
-*/
+
 
 // path_cost_calculator #(.PATH_DELAY(1665)) diagonal_left_to_right ()
 // path_cost_calculator #(.PATH_DELAY(1664)) top_to_bottom ()
 // path_cost_calculator #(.PATH_DELAY(1663)) diagonal_right_to_left ()
 
-localparam TOTAL_COST_BITS = ACC_COST_BITS + 1;
-//localparam TOTAL_COST_BITS = ACC_COST_BITS + 2; // 3-4 paths
+//localparam TOTAL_COST_BITS = ACC_COST_BITS; // 1 path
+//localparam TOTAL_COST_BITS = ACC_COST_BITS + 1; // 2 paths
+localparam TOTAL_COST_BITS = ACC_COST_BITS + 2; // 3-4 paths
 localparam TOTAL_COST_ARR_WIDTH = TOTAL_COST_BITS*DISPARITY_RANGE;
 wire [TOTAL_COST_ARR_WIDTH-1:0] S_array_total;
 generate
 for (d = 0; d <= MAX_DISP; d = d + 1) begin : sum_path_costs
     assign S_array_total[TOTAL_COST_BITS*d + (TOTAL_COST_BITS-1) : TOTAL_COST_BITS*d] =
         L_array_horizontal   [ACC_COST_BITS*d + (ACC_COST_BITS-1) : ACC_COST_BITS*d] +
-        L_array_left_to_right[ACC_COST_BITS*d + (ACC_COST_BITS-1) : ACC_COST_BITS*d];
+        L_array_left_to_right[ACC_COST_BITS*d + (ACC_COST_BITS-1) : ACC_COST_BITS*d] +
+        L_array_top_to_bottom[ACC_COST_BITS*d + (ACC_COST_BITS-1) : ACC_COST_BITS*d] +
+        L_array_right_to_left[ACC_COST_BITS*d + (ACC_COST_BITS-1) : ACC_COST_BITS*d];
 end
 endgenerate
 
